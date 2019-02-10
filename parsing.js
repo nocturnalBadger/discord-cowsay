66d9fb75770c03fc85402aab63b640c32daccb26
Hank H <47288270+Aconka@users.noreply.github.com>
2019-02-10 15:02:18 -0500

    just a start to showing my parsing code
    note: code not included

---
+++ parsing.js
@@ -0,0 +1,13 @@
+function parsing(input) //constructor expects the mesage comeing in minus the cowsay at the begining
+{
+  //put in parsing code its on my other pc
+}
+//setting up vars
+var text;
+var cow;
+var eyes;
+var tongue;
+var wrap;
+var wrapLength;
+var mode;
+//end of setting up vars
